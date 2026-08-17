f00155e470a09e4196c6abdd9dbfc297b3660805
NOME <EMAIL>
2026-08-17 08:56:18 -0300

    Initial commit

---
+++ appSaude/src/main/java/com/br/appSaude/AppSaudeApplication.java
@@ -0,0 +1,13 @@
+package com.br.appSaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSaudeApplication.class, args);
+	}
+
+}
---
+++ appSaude/src/test/java/com/br/appSaude/AppSaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.appSaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
