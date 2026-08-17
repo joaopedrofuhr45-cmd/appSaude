96a183cd9f2235100e6d6a0fbaa3a5e2bcbbdf80
NOME <EMAIL>
2026-08-17 10:32:16 -0300

    terminando os models dos modulos

--- appSaude/src/main/java/com/br/appSaude/modules/atendente/EntittyJpaAtendente.java
+++ appSaude/src/main/java/com/br/appSaude/modules/atendente/EntittyJpaAtendente.java
@@ -1,4 +1,28 @@
 package com.br.appSaude.modules.atendente;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "Atendente")
 public class EntittyJpaAtendente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idAtendente")
+    private Long idAtendente;
+
+    @Column(name = "cpfAtendente", length = 11, nullable = false)
+    private String cpfAtendente;
+
+    @Column(name = "senhaAtendente", length = 255, nullable = false)
+    private String senhaAtendente;
+
+    @Column(name = "nomeAtendente", length = 150, nullable = false)
+    private String nomeAtendente;
 }
--- appSaude/src/main/java/com/br/appSaude/modules/consultas/EntityJpaConsultas.java
+++ appSaude/src/main/java/com/br/appSaude/modules/consultas/EntityJpaConsultas.java
@@ -1,4 +1,48 @@
 package com.br.appSaude.modules.consultas;
 
+import com.br.appSaude.modules.medico.EntityJpaMedico;
+import com.br.appSaude.modules.paciente.EntityJpaPaciente;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "consutlas")
 public class EntityJpaConsultas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idConsulta")
+    private Long idConsulta;
+
+    @Column(name = "dataConsulta", nullable = false)
+    private LocalDate dataConsulta;
+
+    @Column(name = "horaConsulta", nullable = false)
+    private LocalTime horaConsulta;
+
+    @Column(name = "statusConsulta", length = 20)
+    private String statusConsulta;
+
+    @Column(name = "observacoesConsulta", columnDefinition = "TEXT")
+    private String observacoesConsulta;
+
+    @Column(name = "criado_emConsulta", updatable = false)
+    private LocalDateTime criadoEmConsulta;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pacientes_idPaciente", nullable = false)
+    private EntityJpaPaciente paciente;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medico_idMedico", nullable = false)
+    private EntityJpaMedico medico;
+
 }
