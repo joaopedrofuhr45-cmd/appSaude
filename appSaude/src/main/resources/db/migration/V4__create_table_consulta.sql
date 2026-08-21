baefbcbbb016d83e3305b7559f59d5f2f86380fd
João Pedro Fuhr <joaopedrofuhr45@gmail.com>
2026-08-21 11:37:51 -0300

    Add atendente_idAtendente column to consulta table

--- appSaude/src/main/resources/db/migration/V4__create_table_consulta.sql
+++ appSaude/src/main/resources/db/migration/V4__create_table_consulta.sql
@@ -1,18 +1,23 @@
 CREATE TABLE consulta (
-                          idConsulta BIGINT AUTO_INCREMENT PRIMARY KEY,
-                          dataConsulta DATE NOT NULL,
-                          horaConsulta TIME NOT NULL,
-                          statusConsulta VARCHAR(20),
-                          observacoesConsulta TEXT,
-                          criado_emConsulta TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                          pacientes_idPaciente BIGINT NOT NULL,
-                          medico_idMedico BIGINT NOT NULL,
+                        idConsulta BIGINT AUTO_INCREMENT PRIMARY KEY,
+                        dataConsulta DATE NOT NULL,
+                        horaConsulta TIME NOT NULL,
+                        statusConsulta VARCHAR(20),
+                        observacoesConsulta TEXT,
+                        criado_emConsulta TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        pacientes_idPaciente BIGINT NOT NULL,
+                        medico_idMedico BIGINT NOT NULL,
+                        atendente_idAtendente BIGINT NOT NULL, -- Adicionado a coluna do atendente
 
-                          CONSTRAINT fk_consulta_paciente
-                              FOREIGN KEY (pacientes_idPaciente) REFERENCES paciente(idPaciente)
-                                  ON DELETE CASCADE ON UPDATE CASCADE,
+                        CONSTRAINT fk_consulta_paciente
+                            FOREIGN KEY (pacientes_idPaciente) REFERENCES paciente(idPaciente)
+                                ON DELETE CASCADE ON UPDATE CASCADE,
 
-                          CONSTRAINT fk_consulta_medico
-                              FOREIGN KEY (medico_idMedico) REFERENCES medico(idMedico)
-                                  ON DELETE CASCADE ON UPDATE CASCADE
+                        CONSTRAINT fk_consulta_medico
+                            FOREIGN KEY (medico_idMedico) REFERENCES medico(idMedico)
+                                ON DELETE CASCADE ON UPDATE CASCADE,
+
+                        CONSTRAINT fk_consulta_atendente
+                            FOREIGN KEY (atendente_idAtendente) REFERENCES atendente(idAtendente)
+                                ON DELETE CASCADE ON UPDATE CASCADE
 );
